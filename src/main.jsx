82f088d1f66c73a829b406cd34837e013746d968
Saikat Sundar Das <92164254+Saikatssd@users.noreply.github.com>
2024-07-26 18:13:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-export const server = 'http://localhost:5566'
+export const server = 'https://bhashantar-backend.onrender.com/'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
